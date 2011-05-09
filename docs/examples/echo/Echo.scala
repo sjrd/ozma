8fa4619f73c97985d719a628f4305bd10970722e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2011-05-09 22:11:33 +0200

    Rewritten the Echo example with while loops.

--- docs/examples/echo/Echo.scala
+++ docs/examples/echo/Echo.scala
@@ -1,12 +1,9 @@
 object Echo {
   def main(args: Array[String]) {
-    def loop(i: Int) {
-      if (i < args.length) {
-        Console.println(args(i))
-        loop(i+1)
-      }
+    var i = 0
+    while (i < args.length) {
+      Console.println(args(i))
+      i += 1
     }
-
-    loop(0)
   }
 }
