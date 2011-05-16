732649af45cd8fa241cccbd4721d21a2cf5d3a48
Sébastien Doeraene <sjrdoeraene@gmail.com>
2011-05-16 21:03:09 +0200

    Moved so called class-assembly and module-assembley from GenOzCode to GenMozart.

--- src/compiler/scala/tools/nsc/ozma/OzmaTrees.scala
+++ src/compiler/scala/tools/nsc/ozma/OzmaTrees.scala
@@ -4,6 +4,5 @@ package ozma
 import ast.Trees
 
 trait OzmaTrees extends Trees { self: OzmaGlobal =>
-  case class OzCodeClasses(classes: List[ozcodes.ast.ClassDef],
-      modules: List[ozcodes.ast.Eq]) extends Tree
+  case class OzCodeClasses(classes: List[ozcodes.OzClass]) extends Tree
 }
