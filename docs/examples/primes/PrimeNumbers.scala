6c3cdcfaad083e847338583969e5a1d800f284b2
Sébastien Doeraene <sjrdoeraene@gmail.com>
2011-05-17 18:36:01 +0200

    Added an example program that computes the first N prime numbers.
    This program makes intesive use of lazy functions and tail-recursion of ::.

---
+++ docs/examples/primes/PrimeNumbers.scala
@@ -0,0 +1,53 @@
+/**
+ * This program must be given an integer N as command line parameter.
+ * It will display the N first prime numbers.
+ * It computes them using the sieve of Eratosthenes, with lazy functions
+ * working with lists.
+ * Note that all this does work because :: is tail-recursive.
+ */
+
+import ozma._
+import scala.ozma._
+
+object PrimeNumbers {
+  def main(args: Array[String]) {
+    val count = java.lang.Integer.parseInt(args(0))
+    val result = sieve(generateFrom(2))
+    displayFirstN(result, count)
+  }
+  
+  def generateFrom(from: Int): List[Int] = byNeedFuture {
+    from :: generateFrom(from + 1)
+  }
+  
+  def sieve(list: List[Int]): List[Int] = byNeedFuture {
+    list match {
+      case Nil => Nil
+      case head :: tail =>
+        head :: sieve(lazyFilter(tail, _ % head != 0))
+    }
+  }
+  
+  def lazyFilter(list: List[Int],
+      predicate: Int => Boolean): List[Int] = byNeedFuture {
+    list match {
+      case Nil => Nil
+      case head :: tail =>
+        if (predicate(head))
+          head :: lazyFilter(tail, predicate)
+        else
+          lazyFilter(tail, predicate)
+    }
+  }
+
+  def displayFirstN(list: List[Any], count: Int) {
+    if (count > 0) {
+      list match {
+        case Nil => ()
+        case head :: tail =>
+          Console.println(head)
+          displayFirstN(tail, count-1)
+      }
+    }
+  }
+}
