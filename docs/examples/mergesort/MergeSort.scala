dfa3ab64d5e30788fe34c77d661e552fca53b884
Sébastien Doeraene <sjrdoeraene@gmail.com>
2011-06-10 08:46:04 +0200

    Added an example program doing a merge sort on lists.

---
+++ docs/examples/mergesort/MergeSort.scala
@@ -0,0 +1,38 @@
+import scala.ozma._
+
+object MergeSort {
+  def main(args: Array[String]) {
+    val list = args.toList map (_.toInt)
+    val sorted = mergeSort(list)
+    sorted foreach println
+  }
+
+  def mergeSort[A](list: List[A])(
+      implicit cmp: Ordering[_ >: A]): List[A] = {
+    list match {
+      case Nil => Nil
+      case head :: Nil => list
+      case _ =>
+        val (left, right) = split(list)
+        merge(mergeSort(left), mergeSort(right))
+    }
+  }
+
+  def split[A](list: List[A], leftAcc: List[A] = Nil,
+      rightAcc: List[A] = Nil): (List[A], List[A]) = {
+    if (list.isEmpty) (leftAcc, rightAcc)
+    else split(list.tail, list.head :: rightAcc, leftAcc)
+  }
+
+  def merge[A](left: List[A], right: List[A])(
+      implicit cmp: Ordering[_ >: A]): List[A] = {
+    if (left.isEmpty) right
+    else if (right.isEmpty) left
+    else {
+      if (cmp.lteq(left.head, right.head))
+        left.head :: merge(left.tail, right)
+      else
+        right.head :: merge(left, right.tail)
+    }
+  }
+}
