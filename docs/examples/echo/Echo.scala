5fa5bb6fe2ffec4769e0aeb71361e4136687b852
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-11 07:06:07 +0800

    Delete docs/examples directory

--- docs/examples/echo/Echo.scala
+++
@@ -1,9 +0,0 @@
-object Echo {
-  def main(args: Array[String]) {
-    var i = 0
-    while (i < args.length) {
-      Console.println(args(i))
-      i += 1
-    }
-  }
-}
--- docs/examples/helloworld/helloworld/HelloWorld.scala
+++
@@ -1,7 +0,0 @@
-package helloworld
-
-object HelloWorld {
-  def main(args: Array[String]) {
-    Console.println("Hello world!")
-  }
-}
